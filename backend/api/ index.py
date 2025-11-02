c4ab793c355ce7087ca2c46f96235946ab565aac
yumi <yumisunky@gmail.com>
2025-11-02 21:26:38 +0800

    deploy to vercel4

--- backend/api/ index.py
+++ backend/api/ index.py
@@ -1,4 +1,4 @@
-from vercel_asgi import VercelASGI
-from ..main import app  
-
-handler = VercelASGI(app)
+try:
+    from ..main import app   # 推薦：backend/ 和 backend/api/ 都有 __init__.py
+except ImportError:
+    from main import app  
