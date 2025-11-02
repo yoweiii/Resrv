dcd57b181500e1ae6a4fc23dea1f8bcd1fa6d540
yumi <yumisunky@gmail.com>
2025-11-02 21:07:56 +0800

    deploy to vercel2

--- backend/main.py
+++ backend/main.py
@@ -8,7 +8,7 @@ app = FastAPI()
 origins = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
-    "https://resrv.vercel.app",
+    "https://resrv.vercel.app/",
 ]
 app.add_middleware(
     CORSMiddleware,
